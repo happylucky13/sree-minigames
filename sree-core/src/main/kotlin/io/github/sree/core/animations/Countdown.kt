1fa3503ef1e746d7ffee2ecdf2fb2a6e5cd35246
sree <happyluckypotato@gmail.com>
2026-08-18 14:54:57 -0400

    Created Countdown and CountdownManager for simple title countdowns.

---
+++ sree-core/src/main/kotlin/io/github/sree/core/animations/Countdown.kt
@@ -0,0 +1,47 @@
+package io.github.sree.core.animations
+
+import kotlinx.coroutines.delay
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.title.Title
+import org.bukkit.Bukkit
+import org.bukkit.Sound
+import java.util.UUID
+import kotlin.math.roundToInt
+import kotlin.time.Duration.Companion.milliseconds
+
+class Countdown(
+    val totalSeconds: Int,
+    val textColor: NamedTextColor,
+    val sound: Sound = Sound.INTENTIONALLY_EMPTY
+) {
+
+    suspend fun run(playerIds: Collection<UUID>) {
+        val startTime = System.nanoTime()
+        var lastSecond = -1
+
+        while(true) {
+            val elapsed = (System.nanoTime() - startTime) / 1_000_000_000.0
+            val remaining = totalSeconds - elapsed
+            val second = remaining.toInt()
+
+            if (remaining <= 0) return
+            if (second == lastSecond) continue
+
+            lastSecond = second
+
+            val title = Title.title(
+                Component.text(remaining.roundToInt(), textColor),
+                Component.empty()
+            )
+
+            playerIds.forEach { uuid ->
+                val player = Bukkit.getPlayer(uuid)
+                player?.showTitle(title)
+                player?.playSound(player.location, sound, 1.0f, 1.0f)
+            }
+
+            delay(50.milliseconds)
+        }
+    }
+}

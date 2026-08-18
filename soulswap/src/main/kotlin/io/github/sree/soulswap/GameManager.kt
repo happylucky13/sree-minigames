73ebc6ac92dfd0b33046f1202b6c43238f581553
sree <happyluckypotato@gmail.com>
2026-08-18 01:09:44 -0400

    Created incredibly basic GameManager

---
+++ soulswap/src/main/kotlin/io/github/sree/soulswap/GameManager.kt
@@ -0,0 +1,11 @@
+package io.github.sree.soulswap
+
+import io.github.sree.core.SreeCorePlugin
+
+internal class GameManager(
+    val gameState: GameState,
+    val plugin: SoulSwapPlugin,
+    val core: SreeCorePlugin
+) {
+
+}

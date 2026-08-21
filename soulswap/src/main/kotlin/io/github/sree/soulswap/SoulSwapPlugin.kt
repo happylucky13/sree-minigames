59b54ed0ca14df352a3d1a4499a353c10720ce8f
sree <happyluckypotato@gmail.com>
2026-08-21 16:28:36 -0400

    Created a clearScoreboard() method

--- soulswap/src/main/kotlin/io/github/sree/soulswap/SoulSwapPlugin.kt
+++ soulswap/src/main/kotlin/io/github/sree/soulswap/SoulSwapPlugin.kt
@@ -5,6 +5,7 @@ import io.github.sree.soulswap.commands.StartCommand
 import io.github.sree.soulswap.state.GameState
 import io.papermc.paper.command.brigadier.Commands
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
+import org.bukkit.Bukkit
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -12,13 +13,14 @@ class SoulSwapPlugin : JavaPlugin() {
 
     override fun onEnable() {
         logger.info("Plugin initialized")
+        clearScoreboard()
 
         val sreeCore = server.pluginManager.getPlugin("sree-core") as SreeCorePlugin
         val gameState = GameState()
         val gameManager = GameManager(gameState, this, sreeCore)
 
         val listeners: List<Listener> = listOf(
-            PlayerDeathListener(gameManager, gameState)
+            PlayerDeathListener(gameManager, gameState),
         )
 
         listeners.forEach { listener -> server.pluginManager.registerEvents(listener, this) }
@@ -30,5 +32,12 @@ class SoulSwapPlugin : JavaPlugin() {
         this.lifecycleManager.registerEventHandler(LifecycleEvents.COMMANDS) { commands ->
             commands.registrar().register(soulswapCommand)
         }
+    }
+
+    internal fun clearScoreboard() {
+        val scoreboard = Bukkit.getScoreboardManager().mainScoreboard
+        scoreboard.getObjective("lives_left")?.unregister()
+        scoreboard.getTeam("Survivors")?.unregister()
+        scoreboard.getTeam("Purgatory")?.unregister()
     }
 }

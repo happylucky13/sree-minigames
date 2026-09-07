386044533e36be51ad18ea3e38a49c64b3d2f83b
sree <happyluckypotato@gmail.com>
2026-09-06 23:27:12 -0400

    Created yaml file

--- advancement-lockout/build.gradle.kts
+++ advancement-lockout/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "io.github.sree.lockout"
-version = "0.5.0"
+version = "0.1.0"
 
 tasks.processResources {
     val props = mapOf("version" to version)
---
+++ advancement-lockout/src/main/kotlin/io/github/sree/lockout/AdvancementLockoutPlugin.kt
@@ -0,0 +1,10 @@
+package io.github.sree.lockout
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class AdvancementLockoutPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        super.onEnable()
+    }
+}
